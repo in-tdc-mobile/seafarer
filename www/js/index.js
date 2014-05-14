9d1b40a901fce735f271c0be531cb00e1880a861
sandeepjr <sandeep.ramachandran@bs-shipmanagement.com>
2014-05-14 11:22:07 +0530

    push notification added

--- www/js/index.js
+++ www/js/index.js
@@ -64,6 +64,47 @@ var app = {
     }
 };
 
+function register_push_service() {
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"1075090837516","ecb":"app.onNotificationGCM"});
+
+}
+
+function successHandler (result) {
+    alert('Callback Success! Result = '+result)
+}
+
+// result contains any error description text returned from the plugin call
+function errorHandler (error) {
+    alert(error);
+}
+
+onNotificationGCM: function(e) {
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                console.log("Regid " + e.regid);
+                alert('registration id = '+e.regid);
+            }
+        break;
+
+        case 'message':
+          // this is the actual push notification. its format depends on the data model from the push server
+          alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+        break;
+
+        case 'error':
+          alert('GCM error = '+e.msg);
+        break;
+
+        default:
+          alert('An unknown GCM event has occurred');
+          break;
+    }
+}
+
 // Handle the back button
 //
 function onBackKeyDown() {
@@ -530,7 +571,7 @@ function openpositions(){
     });
 }
 
-function show_flight_details() {
+function show_flight_details() { alert("");
     $("#index_content").hide();
     $('#show_flight_details').show(); 
     var url = prefilurl+"get_sf_flight_details.php?empid="+$.jStorage.get("empid");
