5b5676bc2341f6c2db053c3d8fdc22676d20811f
sandeepjr <sandeep.ramachandran@bs-shipmanagement.com>
2014-07-04 16:37:24 +0530

    tick marck for read message

--- www/js/index.js
+++ www/js/index.js
@@ -992,7 +992,10 @@ function getcorrespondance() {
             var d = new Date();
             if(data[0] != null) {
                 for (var i = 0; i < data.length; i++) {
-                    results_array.push("<p class='triangle-right left' style='word-wrap:break-word;'>"+data[i]['message']+"</p>");
+                    results_array.push("<p class='triangle-right left' style='word-wrap:break-word;'>"+data[i]['message']);
+                    if(data[i]['is_read'] == 'Y')
+                        results_array.push("<img src = 'img/check-mark-md.png' style='float: right;'>");
+                    results_array.push("</p>");
 
                 }
             }
